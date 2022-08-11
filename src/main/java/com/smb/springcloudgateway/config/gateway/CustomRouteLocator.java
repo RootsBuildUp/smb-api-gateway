3b3e5bfbdcc54c98fb589fa14e2a9d9653f3c0cc
RootsbuildUp <rashedfci994@gmail.com>
2022-08-12 01:05:27 +0600

    now corss origin stable

--- src/main/java/com/smb/springcloudgateway/config/gateway/CustomRouteLocator.java
+++ src/main/java/com/smb/springcloudgateway/config/gateway/CustomRouteLocator.java
@@ -2,27 +2,21 @@ package com.smb.springcloudgateway.config.gateway;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
-import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
 
-import javax.ws.rs.HttpMethod;
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * <a href="https://www.baeldung.com/spring-cloud-gateway#routing-factories">CustomRouteLocator</a>
  * <p><a href="https://www.tabnine.com/code/java/methods/org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder/routes">Example</a></p>
  * <p><a href="https://www.baeldung.com/spring-cloud-gateway">spring-cloud-gateway</a></p>
  * <p>2. Routing Handler</p>
- * <p>Being focused on routing requests, the Spring Cloud Gateway forwards requests to a Gateway
- * Handler Mapping – which determines what should be done with requests matching a specific
- * route.</p>
+ *<p>Being focused on routing requests, the Spring Cloud Gateway forwards requests to a Gateway
+ *  Handler Mapping – which determines what should be done with requests matching a specific
+ *  route.</p>
  * <p>Let's start with a quick example of how to the Gateway Handler
  * resolves route configurations by using RouteLocator
  * </p>
@@ -33,29 +27,15 @@ import java.util.HashMap;
 @Configuration
 public class CustomRouteLocator {
 
-    @Bean
-    public CorsConfiguration corsConfiguration(RoutePredicateHandlerMapping routePredicateHandlerMapping) {
-        System.err.println("================ route predicate handler ===================");
-        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
-        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST)
-                .forEach(m -> corsConfiguration.addAllowedMethod(m));
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<>() {{
-            put("/**", corsConfiguration);
-        }});
-        return corsConfiguration;
-    }
 
     @Bean
     public RouteLocator customGatewayRoutes(RouteLocatorBuilder builder) {
-        System.err.println("================ Route Locator Builder ===================");
         return builder.routes()
                 .route(r -> r.path("/um-module/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("um-module") // circuit breaker
                                 .setFallbackUri("/defaultfallback")))
                         .uri("lb://um-module"))// If the URL has a lb scheme (e.g., lb://um-module), it'll use the Spring Cloud LoadBalancerClient to resolve the name (i.e., um-module) to an actual host and port.
-                .route(r -> r.path("/remittance-module/**")
+              .route(r -> r.path("/remittance-module/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("remittance-module") // circuit breaker
                                 .setFallbackUri("/defaultfallback")))
                         .uri("lb://remittance-module"))
--- src/main/java/com/smb/springcloudgateway/config/gateway/GatewayDiscoveryConfiguration.java
+++ src/main/java/com/smb/springcloudgateway/config/gateway/GatewayDiscoveryConfiguration.java
@@ -1,7 +1,6 @@
 package com.smb.springcloudgateway.config.gateway;
 
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
